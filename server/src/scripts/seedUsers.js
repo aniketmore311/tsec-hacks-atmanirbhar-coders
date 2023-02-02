32fd8d81167aefe6256548de0a4bba3770a4ddbd
aniket more <aniketavinashmore343@gmail.com>
2023-02-03 05:12:21 +0530

    feat: modify seed script

--- server/src/scripts/seedUsers.js
+++ server/src/scripts/seedUsers.js
@@ -26,7 +26,7 @@ async function main() {
     userId: user1.id,
     age: 23,
     bio: "backend developer",
-    city: "Mumbai",
+    city: "Thane",
     state: "Maharastra",
     country: "India",
     field: "Computer Engineering",
@@ -35,14 +35,14 @@ async function main() {
     gender: "male",
     type: "student",
     profilePictureURL:
-      "https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp",
+      "https://scontent.fbom8-1.fna.fbcdn.net/v/t31.18172-8/15732054_1829744933948321_6912147507579640372_o.png?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=NqMfrZyunEkAX9lVA4R&_nc_oc=AQlOYEgINbsXv40PdRJrPLdSKGlQWwW1ALQNVYt3YiKDdJHZPwjwCeh-BCUPoSyS8_M&_nc_ht=scontent.fbom8-1.fna&oh=00_AfBCdN5ZG-o21IBPex2ZRQi2Mg_wvpDVEsTtMQBneQY8ng&oe=6403AB7B",
     institute: "tsec bandra",
     interests: ["reading", "travelling"],
     locality: "thane",
     maritalStatus: "single",
     smokingPreference: "no",
     socialMediaLinks: [
-      "www.facebook.com/profile/yash",
+      "www.facebook.com/profile/aniket",
       "www.instagram.com/profile/aniket",
       "www.linkedin.com/profile/aniket",
     ],
@@ -63,7 +63,7 @@ async function main() {
   const profile2 = await Profile.create({
     userId: user2.id,
     age: 20,
-    bio: "backend developer",
+    bio: "react developer",
     city: "Mumbai",
     state: "Maharastra",
     country: "India",
@@ -81,8 +81,8 @@ async function main() {
     smokingPreference: "no",
     socialMediaLinks: [
       "www.facebook.com/profile/yash",
-      "www.instagram.com/profile/aniket",
-      "www.linkedin.com/profile/aniket",
+      "www.instagram.com/profile/yash",
+      "www.linkedin.com/profile/yash",
     ],
   });
   const user3 = await User.create({
@@ -101,13 +101,13 @@ async function main() {
   const profile3 = await Profile.create({
     userId: user3.id,
     age: 20,
-    bio: "backend developer",
+    bio: "front end developer",
     city: "Mumbai",
     state: "Maharastra",
     country: "India",
     field: "Computer Engineering",
-    dateOfBirth: "2002-04-04",
-    foodPreference: "veg",
+    dateOfBirth: "2001-03-04",
+    foodPreference: "non-veg",
     gender: "male",
     type: "student",
     profilePictureURL:
@@ -139,13 +139,13 @@ async function main() {
   const profile4 = await Profile.create({
     userId: user4.id,
     age: 20,
-    bio: "backend developer",
+    bio: "react developer developer",
     city: "Mumbai",
     state: "Maharastra",
     country: "India",
     field: "Computer Engineering",
-    dateOfBirth: "2002-04-04",
-    foodPreference: "veg",
+    dateOfBirth: "2001-12-12",
+    foodPreference: "non-veg",
     gender: "male",
     type: "student",
     profilePictureURL:
@@ -154,10 +154,49 @@ async function main() {
     interests: ["sports", "coding", "trekking"],
     locality: "borivali",
     maritalStatus: "single",
+    smokingPreference: "no",
     socialMediaLinks: [
-      "www.facebook.com/profile/akash",
-      "www.instagram.com/profile/akash",
-      "www.linkedin.com/profile/akash",
+      "www.facebook.com/profile/chinmay",
+      "www.instagram.com/profile/chinmay",
+      "www.linkedin.com/profile/chinmay",
+    ],
+  });
+  const user5 = await User.create({
+    firstname: "john",
+    lastname: "doe",
+    email: "johndoe@gmail.com",
+    phoneNumber: "1234567890",
+    emailOTP: "123",
+    phoneNumberOTP: "123",
+    isEmailVerified: true,
+    isPhoneNumberVerified: true,
+    password: hashedPassword,
+    role: "user",
+    isProfileCreated: true,
+  });
+  const profile5 = await Profile.create({
+    userId: user5.id,
+    age: 20,
+    bio: "accountant",
+    city: "Delhi",
+    state: "Delhi",
+    country: "India",
+    field: "Computer Engineering",
+    dateOfBirth: "1995-12-12",
+    foodPreference: "non-veg",
+    gender: "male",
+    type: "professional",
+    profilePictureURL:
+      "https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp",
+    company: "kpmg",
+    interests: ["sports", "coding", "trekking"],
+    locality: "delhi",
+    maritalStatus: "married",
+    smokingPreference: "no",
+    socialMediaLinks: [
+      "www.facebook.com/profile/john",
+      "www.instagram.com/profile/john",
+      "www.linkedin.com/profile/john",
     ],
   });
   await mongoose.disconnect();
